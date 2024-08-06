c258c23ecfeaba85a7fb9e04d355997aacc01bac
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-06 14:29:12 +1000

    Range fix

--- src/toaster_utils/scripts/user.py
+++ src/toaster_utils/scripts/user.py
@@ -76,7 +76,7 @@ def get_user_warns(session: Session, uuid: int, bpid: int) -> Optional[WarnInfo]
 @script(auto_commit=False, debug=True)
 def set_user_warns(session: Session, uuid: int, bpid: int, points: int) -> None:
     warn = session.get(Warn, {"bpid": bpid, "uuid": uuid})
-    if points <= 0 or points > 10:
+    if points <= 0 or points >= 10:
         session.delete(warn)
     else:
         warn.points = points
