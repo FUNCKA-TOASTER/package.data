27ddf17b26d32a672f6a468adb600d32bc791df8
Oidaho <76991612+Oidaho@users.noreply.github.com>
2024-08-26 02:24:16 +1000

    Updating the names of environment variables.

--- src/toaster/config.py
+++ src/toaster/config.py
@@ -2,14 +2,14 @@ import os
 from funcka_bots.credentials import AlchemyCredentials, AlchemySetup
 
 SQL_ALCHEMY_SETUP = AlchemySetup(
-    dialect="mysql",
-    driver="pymysql",
-    database=os.getenv("DATABASE"),
+    dialect=os.getenv("alchemy_dialect"),
+    driver=os.getenv("alchemy_driver"),
+    database=os.getenv("alchemy_database"),
 )
 
 SQL_ALCHEMY_CREDS = AlchemyCredentials(
-    host=os.getenv("SQL_HOST"),
-    port=int(os.getenv("SQL_PORT")),
-    user=os.getenv("SQL_USER"),
-    pswd=os.getenv("SQL_PSWD"),
+    host=os.getenv("sql_host"),
+    port=int(os.getenv("sql_port")),
+    user=os.getenv("sql_user"),
+    pswd=os.getenv("sql_pswd"),
 )
